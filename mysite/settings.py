9c03d24b24d5123f6d0e65678a1dfad9ab327572
SergijK <101892845+RoBoGeRR@users.noreply.github.com>
2024-08-11 20:32:55 +0000

    .

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 CSRF_TRUSTED_ORIGINS = [
-    '*',
+    # 'https://blogdjango-production-d79a.up.railway.app/',
+    'https://blogdjango-production'
     # Add other trusted origins here if needed.
 ]
 # Quick-start development settings - unsuitable for production
